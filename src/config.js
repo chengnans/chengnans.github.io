f35496e66c05d5cd9c01cf7b4928677f64678b2e
chengnans <124565640+chengnans@users.noreply.github.com>
2023-12-31 21:02:47 +0800

    Update config.js 狗儿

--- src/config.js
+++ src/config.js
@@ -7,9 +7,9 @@ export default {
   // page2部分
   page2: {
     authorImg: 'page1.jpg', // 作者头像
-    xinhui: '我叫卫婷婷，是一名在读的大三学生。', // 幸会
+    xinhui: 'chengnan，在读大学生。', // 幸会
     qiuzhi: '前端开发', // 求职意向
-    guanyuwo: '掌握了前端js、html、css基本技术和Vue框架、UI框架，nodejs等等，有着公司项目实战的经验和一些java安卓开发经历。希望可以和大家一起学习，共同进步！' // 关于我
+    guanyuwo: '掌握了前端js、html、css基本技术和Vue框架、UI框架，nodejs等等，希望可以和大家一起学习，共同进步！' // 关于我
   },
   // page3部分
   page3: [{
@@ -33,24 +33,25 @@ export default {
   page4: {
     // 我的历程
     course: [{
-      date: '2020/9——至今', // 时间
+      date: '2022/9——至今', // 时间
       desc: { // 经历
-        title: '山西传媒学院',
-        list: ['山西省职业技能大赛web前端开发一等奖', '专升本上岸', '软件开发公司实习']
+        title: '龙专',
+        list: ['吹牛一等奖']
       }
-    }, {
-      date: '2019/9——2020/9',
+    }, 
+    {
+      date: '从古至今',
       desc: {
-        title: '晋城职业技术学院',
-        list: ['学习前端语言', '构建页面 / 利用JS、CSS3等制作网站，vue有关项目', '维护508工作室','获得Web前段开发1+X证书','普通话二级甲等','国家励志奖学金']
+        title: '暴打狗儿高照轩',
+        list: ['暴打狗儿']
       }
-    }, {
+    }, /*{
       date: '2018/9——2019/9',
       desc: {
         title: '晋城职业技术学院',
         list: ['学习java语言', '利用Android、sql等制作app', '参加山西省职业院校移动互联技能大赛', '全国计算机等级二级证书', '国家励志奖学金']
       }
-    }],
+    }*/],
     // 我的拓展技能掌握
     singlelist: [{
       title: 'bootsrap',
@@ -84,6 +85,7 @@ export default {
     }]
   },
   // page5部分
+  /*
   page5: [{
     title: '508工作室',
     content: '晋城职业技术学院508工作室独立站点的维护',
@@ -114,12 +116,12 @@ export default {
     content: 'Android二维码生成器',
     image: 'box6.jpeg',
     href: ''
-  }],
+  }],*/
   // page6部分
   page6: {
-    github: 'https://github.com/wttAndroid',
+    github: 'https://github.com/chengnans',
     bili: '',
-    email: 'Mailto:1457321681@qq.com?Subject=邮箱标题&Body=邮箱内容！',
+    email: 'Mailto:mail@chengnan.me?Subject=邮箱标题&Body=邮箱内容！',
     zhihu: '',
     weixin: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/weix.jpg' alt='加载失败'>",
     qq: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/qq.jpg' alt='加载失败'>"
